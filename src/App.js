656c887f4c8fd36f752fad87027ee2d68bc28abd
Yechiel Klein <kleinyechiel@gmail.com>
2023-05-21 23:40:44 -0400

    added props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ function App() {
 	return (
 		<div>
 			<div>Personal Digital Assistants</div>
-			
-			<ProfileCard />
-			<ProfileCard />
-			<ProfileCard />
+
+			<ProfileCard
+				name="Alexa"
+				imgSrc=""
+				tag="@alexa99"
+				description="I'll help you buy stuff on Amazon"
+			/>
+			<ProfileCard
+				name="Cortana"
+				imgSrc=""
+				tag="@Cortana12"
+				description="Personal assistant by Microsoft"
+			/>
+			<ProfileCard
+				name="Siri"
+				imgSrc=""
+				tag="@siri44"
+				description="I don't get a lot of updates anymore"
+			/>
 		</div>
 	);
 };
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,12 @@
-function ProfileCard() {
-	return <div>Profile Card</div>;
+function ProfileCard({ name, imgSrc, tag, description }) {
+	return (
+		<div>
+			<img src={imgSrc} />
+			<h1>{name}</h1>
+			<p>{tag}</p>
+			<h4>{description}</h4>
+		</div>
+	);
 };
 
 export default ProfileCard;
